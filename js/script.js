2a64e18f6b70995dc947201e68a7d65047c50490
Popova Olga <686371olia@mail.ru>
2022-02-28 02:17:02 +0300

    Удаление блока с рекламой

--- js/script.js
+++ js/script.js
@@ -2,11 +2,14 @@
 
 const books = document.querySelectorAll('.book');
 const body = document.querySelector('body');
+const adv = document.querySelectorAll('.adv');
 
 books[0].before(books[1]);
 books[5].after(books[2]);
 books[4].after(books[3]);
 
 body.style.backgroundImage = 'url(./image/you-dont-know-js.jpg)';
+
+adv[0].remove();
 
 console.log(books); 
