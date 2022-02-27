dfdc89ea89bdff8c40f2cd9757aa116c0c299d96
Popova Olga <686371olia@mail.ru>
2022-02-28 02:12:19 +0300

    Замена картинки на заднем фоне

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 'use strict';
 
 const books = document.querySelectorAll('.book');
-console.log(books); 
+const body = document.querySelector('body');
 
 books[0].before(books[1]);
 books[5].after(books[2]);
 books[4].after(books[3]);
 
+body.style.backgroundImage = 'url(./image/you-dont-know-js.jpg)';
+
+console.log(books); 
