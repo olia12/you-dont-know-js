11009689c2baba3bbb0821dab088704d2bf2e430
Popova Olga <686371olia@mail.ru>
2022-02-28 02:34:19 +0300

    Исправление заголовока в книге 3

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const books = document.querySelectorAll('.book');
+const bookTitle = document.querySelectorAll('.book h2 a')[4];
 const body = document.querySelector('body');
 const adv = document.querySelectorAll('.adv');
 
@@ -10,6 +11,9 @@ books[4].after(books[3]);
 
 body.style.backgroundImage = 'url(./image/you-dont-know-js.jpg)';
 
+bookTitle.textContent = 'Книга 3. this и Прототипы Объектов';
+
 adv[0].remove();
 
 console.log(books); 
+console.log(bookTitle); 
